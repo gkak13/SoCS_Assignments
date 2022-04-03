b4782629eab146d9e4bc6549a37beccb6cb734d3
gkak13 <93548004+gkak13@users.noreply.github.com>
2022-04-03 13:40:08 +0200

    Update Sugarscape16_5.m

--- Sugarscape16_5.m
+++ Sugarscape16_5.m
@@ -1,5 +1,5 @@
 %% Simulation of Complex Systems                                %%
-%% Kakkos Ioannis 930413-6030                                   %%
+%% Kakkos Ioannis                                               %%
 %% Exercise 16.5 A simple Sugarscape                            %%
 clear all;
 
