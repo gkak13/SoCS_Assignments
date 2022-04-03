a2800394ea4a75f9faca8e304997302d8982e8c9
gkak13 <93548004+gkak13@users.noreply.github.com>
2022-04-03 13:39:39 +0200

    Update Sugarscape16_3.m

--- Sugarscape16_3.m
+++ Sugarscape16_3.m
@@ -1,5 +1,5 @@
 %% Simulation of Complex Systems                                %%
-%% Kakkos Ioannis 930413-6030                                   %%
+%% Kakkos Ioannis                                               %%
 %% Exercise 16.3 Anti-gregarious spirit                         %%
 clear all; 
 
