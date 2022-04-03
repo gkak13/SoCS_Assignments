a1641a59d3dc62c7f2cb0e0554f9d6081cef7e8e
gkak13 <93548004+gkak13@users.noreply.github.com>
2022-04-03 13:40:19 +0200

    Update Sugarscape16_6.m

--- Sugarscape16_6.m
+++ Sugarscape16_6.m
@@ -1,5 +1,5 @@
 %% Simulation of Complex Systems                                  %%
-%% Kakkos Ioannis 930413-6030                                     %%
+%% Kakkos Ioannis                                                 %%
 %% Exercise 16.6 Sugarscape with a finite lifespan for the agents %%
 clear all;
 
