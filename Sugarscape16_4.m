0f1a81636080ead46827b699f87da1ada2ba1620
gkak13 <93548004+gkak13@users.noreply.github.com>
2022-04-03 13:39:56 +0200

    Update Sugarscape16_4.m

--- Sugarscape16_4.m
+++ Sugarscape16_4.m
@@ -1,5 +1,5 @@
 %% Simulation of Complex Systems                                %%
-%% Kakkos Ioannis 930413-6030                                   %%
+%% Kakkos Ioannis                                               %%
 %% Exercise 16.4 Emergence of frustration                       %%
 clear all; 
 
