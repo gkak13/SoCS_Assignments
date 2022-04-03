3c2873af7799b76eb2e47839bafaa15c06ec591f
gkak13 <93548004+gkak13@users.noreply.github.com>
2022-04-03 13:39:25 +0200

    Update Sugarscape16_1.m

--- Sugarscape16_1.m
+++ Sugarscape16_1.m
@@ -1,5 +1,5 @@
 %% Simulation of Complex Systems                                %%
-%% Kakkos Ioannis 930413-6030                                   %%
+%% Kakkos Ioannis                                               %%
 %% Exercise 16.1 Schelling's model of segregation               %%
 clear all; 
 
